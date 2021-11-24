9e5583d67501f3c957ec67a546dfd41cfbaf4dcb
Chad Groft <clgroft@gmail.com>
2021-11-24 10:53:31 -0500

    Set up input files and command line

---
+++ aoc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import click
+from collections import defaultdict
+import sys
+
+
+solutions = defaultdict(lambda : lambda day, lines : print(f"Day {day} not yet implemented"))
+
+
+@click.command()
+@click.option('--day', default=1, help='Run the solution for this day.')
+@click.option('--input_type', default='real', help="Specify input ('real' or 'test')")
+def aoc(day, input_type):
+    """Run the AOC 2021 solution for the given day, on the real or test input."""
+    if input_type != 'real' and input_type != 'test':
+        print("Unrecognized input type (must be 'real' or 'test')")
+        sys.exit(1)
+
+    if day < 1 or day > 25:
+        print("Unrecognized day (must be between 1 and 25 inclusive)")
+        sys.exit(1)
+
+    path = f'{input_type}/day{day:0>2d}.txt'
+    with open(path) as f:
+        lines = f.readlines()
+        solutions[day](day, lines)
+
+
+if __name__ == "__main__":
+    aoc()
