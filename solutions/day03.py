0c563d28bce156b68261475d76c49659727dc3cf
Chad Groft <clgroft@gmail.com>
2021-12-03 07:56:04 -0500

    Cleanup day 3

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,3 +1,24 @@
+def gamma_epsilon(inputs):
+    counts = [ [0,0] for c in inputs[0]]
+    for l in inputs:
+        for i in range(len(l)):
+            counts[i][l[i]] += 1
+
+    gamma_binary = []
+    epsilon_binary = []
+    for c in counts:
+        if c[0] > c[1]:
+            gamma_binary.append('0')
+            epsilon_binary.append('1')
+        else:
+            gamma_binary.append('1')
+            epsilon_binary.append('0')
+
+    gamma = int(''.join(gamma_binary), 2)
+    epsilon = int(''.join(epsilon_binary), 2)
+    return gamma, epsilon
+
+
 def oxygen_generator_rating(inputs):
     for i in range(len(inputs[0])):
         counts = [0,0]
@@ -32,25 +53,7 @@ def solution(day, lines):
     lines = [ [ int(c) for c in l.strip() ] for l in lines ]
 
     print('Part 1:')
-
-    counts = [ [0,0] for c in lines[0]]
-    for l in lines:
-        for i in range(len(l)):
-            counts[i][l[i]] += 1
-
-    gamma_binary = []
-    epsilon_binary = []
-    for c in counts:
-        if c[0] > c[1]:
-            gamma_binary.append('0')
-            epsilon_binary.append('1')
-        else:
-            gamma_binary.append('1')
-            epsilon_binary.append('0')
-
-    gamma = int(''.join(gamma_binary), 2)
-    epsilon = int(''.join(epsilon_binary), 2)
-
+    gamma, epsilon = gamma_epsilon(lines)
     print(f'gamma = {gamma}, epsilon = {epsilon}, answer = {gamma * epsilon}')
 
     print('Part 2:')
