2c8a2b766874bc717a749908d9d1d5311dee709a
Chad Groft <clgroft@gmail.com>
2021-12-06 09:53:47 -0500

    Day 6: add clarifying comments

--- solutions/day06.py
+++ solutions/day06.py
@@ -1,15 +1,25 @@
 def solution(day, lines):
+    """Solution for day 6.
+
+    Input is a single line of integers separated by commas, each
+    representing a single lanternfish; the integer is the number of days
+    to next spawn.  A mature lanternfish spawns every 7 days; a newly
+    spawned lanternfish takes 9 days to spawn the first time.
+
+    Output is the total number of lanternfish after 80 and 256 days.
+    """
+
+    # fish_count[i] == number of lanternfish which will spawn in i days.
+    # E.g. fish_count[0] fish are about to spawn.
     fish_count = [0] * 9
     for word in lines[0].split(','):
         fish_count[int(word)] += 1
 
     for i in range(256):
         new_fish_count = [0] * 9
-        new_fish_count[:8] = fish_count[1:]
-        new_fish_count[6] += fish_count[0]
-        new_fish_count[8] = fish_count[0]
+        new_fish_count[:8] = fish_count[1:] # counting down to next spawn
+        new_fish_count[6] += fish_count[0]  # just spawned, resetting count
+        new_fish_count[8] = fish_count[0]   # newly spawned
         fish_count = new_fish_count
-        if i == 79:  # after 80 days
-            print(f'After 80 days, {sum(fish_count)} fish')
-
-    print(f'After 256 days, {sum(fish_count)} fish')
+        if i == 79 or i == 255:  # after 80 and 256 days
+            print(f'After {i+1} days, {sum(fish_count)} fish')
