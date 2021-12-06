441145c01246a1a9c662d5b823e5c707d84f4c7a
Chad Groft <clgroft@gmail.com>
2021-12-06 10:11:39 -0500

    Day 5: cleanup

--- solutions/day05.py
+++ solutions/day05.py
@@ -13,46 +13,47 @@ class VentLine:
 
     def covered_points_1(self):
         if self.start_x == self.end_x:
-            start_y, end_y = self.start_y, self.end_y
-            if start_y > end_y:
-                start_y, end_y = end_y, start_y
-            return ((self.start_x, y) for y in range(start_y, end_y + 1))
+            return self._vertical_line()
         if self.start_y == self.end_y:
-            start_x, end_x = self.start_x, self.end_x
-            if start_x > end_x:
-                start_x, end_x = end_x, start_x
-            return ((x, self.start_y) for x in range(start_x, end_x + 1))
+            return self._horizontal_line()
         else:
             return []
 
     def covered_points_2(self):
         if self.start_x == self.end_x:
-            start_y, end_y = self.start_y, self.end_y
-            if start_y > end_y:
-                start_y, end_y = end_y, start_y
-            return ((self.start_x, y) for y in range(start_y, end_y + 1))
-
+            return self._vertical_line()
         if self.start_y == self.end_y:
-            start_x, end_x = self.start_x, self.end_x
-            if start_x > end_x:
-                start_x, end_x = end_x, start_x
-            return ((x, self.start_y) for x in range(start_x, end_x + 1))
-
+            return self._horizontal_line()
         else:
-            start_x, end_x = self.start_x, self.end_x
-            start_y, end_y, inc_y = self.start_y, self.end_y, 1
-            if start_x > end_x:
-                start_x, end_x = end_x, start_x
-                start_y, end_y = end_y, start_y
-            if start_y > end_y:
-                inc_y = -1
+            return self._diagonal_line()
 
-            points, x, y = [], start_x, start_y
-            while x <= end_x:
-                points.append((x,y))
-                x += 1
-                y += inc_y
-            return points
+    def _vertical_line(self):
+        start_y, end_y = self.start_y, self.end_y
+        if start_y > end_y:
+            start_y, end_y = end_y, start_y
+        return ((self.start_x, y) for y in range(start_y, end_y + 1))
+
+    def _horizontal_line(self):
+        start_x, end_x = self.start_x, self.end_x
+        if start_x > end_x:
+            start_x, end_x = end_x, start_x
+        return ((x, self.start_y) for x in range(start_x, end_x + 1))
+
+    def _diagonal_line(self):
+        start_x, end_x = self.start_x, self.end_x
+        start_y, end_y, inc_y = self.start_y, self.end_y, 1
+        if start_x > end_x:
+            start_x, end_x = end_x, start_x
+            start_y, end_y = end_y, start_y
+        if start_y > end_y:
+            inc_y = -1
+
+        points, x, y = [], start_x, start_y
+        while x <= end_x:
+            points.append((x,y))
+            x += 1
+            y += inc_y
+        return points
 
 
 def solution(day, lines):
@@ -62,12 +63,12 @@ def solution(day, lines):
     for v in vent_lines:
         for p in v.covered_points_1():
             covered_points[p] += 1
-    num_crossings = len([v for v in covered_points.values() if v > 1])
+    num_crossings = sum(1 for v in covered_points.values() if v > 1)
     print(f'Part 1: There are {num_crossings} crossings')
 
     covered_points = defaultdict(lambda: 0)
     for v in vent_lines:
         for p in v.covered_points_2():
             covered_points[p] += 1
-    num_crossings = len([v for v in covered_points.values() if v > 1])
+    num_crossings = sum(1 for v in covered_points.values() if v > 1)
     print(f'Part 2: There are {num_crossings} crossings')
