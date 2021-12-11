63f36dd7739d5cdbbc33eb35eb7d75181d76f1de
Chad Groft <clgroft@gmail.com>
2021-12-11 09:34:43 -0500

    Day 10: calculate everything in one pass

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from statistics import median
 
 
@@ -7,36 +8,38 @@ SCORE_FOR_CLOSE = {')': 3, ']': 57, '}': 1197, '>': 25137}
 SCORE_FOR_OPEN = {'(': 1, '[': 2, '{': 3, '<': 4}
 
 
-def syntax_error_score(line):
+@dataclass
+class Result:
+    error_type: str
+    score: int
+
+
+def error_type_and_score(line):
     stack = []
     for c in line:
         if c in OPEN_CHARS:
             stack.append(c)
-        elif stack.pop() != OPEN_FOR_CLOSE[c]:  # line is corrupted
-            return SCORE_FOR_CLOSE[c]
-    return 0  # line is incomplete
+        elif stack.pop() != OPEN_FOR_CLOSE[c]:
+            return Result('corrupted', SCORE_FOR_CLOSE[c])
 
+    if not stack:
+        return Result('none', 0)  # this shouldn't actually happen
 
-def autocomplete_score(line):
-    stack = []
-    for c in line:
-        if c in OPEN_CHARS:
-            stack.append(c)
-        elif stack.pop() != OPEN_FOR_CLOSE[c]:  # line is corrupted
-            return None
-
-    # Line is incomplete
     score = 0
     while stack:
         score = score * 5 + SCORE_FOR_OPEN[stack.pop()]
-    return score
+    return Result('incomplete', score)
 
 
 def solution(day, lines):
-    lines = list(map(str.strip, lines))
+    results = list(map(error_type_and_score, map(str.strip, lines)))
 
-    se_score = sum(map(syntax_error_score, lines))
+    se_score = sum(
+        map(lambda r : r.score,
+            filter(lambda r : r.error_type == 'corrupted', results)))
     print(f'Total syntax error score: {se_score}')
 
-    median_ac_score = median(filter(lambda n : n, map(autocomplete_score, lines)))
+    median_ac_score = median(
+        map(lambda r : r.score,
+            filter(lambda r : r.error_type == 'incomplete', results)))
     print(f'Median autocomplete score: {median_ac_score}')
