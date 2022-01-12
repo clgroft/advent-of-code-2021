4b775a6147198f2d79ea6540a1318ba65c7beb73
Chad Groft <clgroft@gmail.com>
2022-01-12 17:01:34 -0500

    Day 19: cleanup

--- solutions/day19.py
+++ solutions/day19.py
@@ -1,11 +1,119 @@
 from collections import Counter
+from dataclasses import dataclass
 from functools import reduce
 import numpy as np
 import re
 
 
-HEADER_LINE = re.compile('^--- scanner (\d+) ---$')
-BEACON_LINE = re.compile('^(-?\d+),(-?\d+),(-?\d+)$')
+def solution(day, lines):
+    interim_solution = InterimSolution(scans(lines))
+    if not interim_solution.find_matches():
+        return
+    print(f'Found {interim_solution.num_beacons()} beacons')
+    print(f'Max Manhattan distance: {interim_solution.max_manhattan_distance()}')
+
+
+def scans(lines):
+    original_scans = []
+    latest_scan = None
+    header_line = re.compile('^--- scanner (\d+) ---$')
+    beacon_line = re.compile('^(-?\d+),(-?\d+),(-?\d+)$')
+
+    for l in lines:
+        m = header_line.match(l)
+        if m:
+            latest_scan = set()
+            continue
+
+        m = beacon_line.match(l)
+        if m:
+            latest_scan.add((int(m.group(1)), int(m.group(2)), int(m.group(3))))
+            continue
+
+        # presumably blank line
+        original_scans.append(latest_scan)
+        latest_scan = None
+
+    if latest_scan is not None:
+        original_scans.append(latest_scan)
+    return original_scans
+
+
+class InterimSolution:
+    def __init__(self, original_scans):
+        self._original_scans = original_scans
+        self._placed_scans = {0: PlacedScan(np.array((0,0,0)), original_scans[0])}
+        self._unplaced_scan_indices = set(range(1, len(original_scans)))
+        self._failed_matches = set()
+
+    def find_matches(self):
+        while self._unplaced_scan_indices:
+            if not self._find_single_match():
+                return False
+        return True
+
+    def num_beacons(self):
+        return len(reduce(set.union,
+                          map(lambda ps: ps.beacons, self._placed_scans.values())))
+
+    def max_manhattan_distance(self):
+        return max(sum(abs(ps1.position - ps2.position))
+                   for ps1 in self._placed_scans.values()
+                   for ps2 in self._placed_scans.values())
+
+    def _find_single_match(self):
+        for i in sorted(self._unplaced_scan_indices):
+            original_scan = self._original_scans[i]
+            for j, placed_scan in self._placed_scans.items():
+                if (i,j) in self._failed_matches:
+                    continue
+                result = try_to_fit(original_scan, placed_scan)
+                if result:
+                    self._placed_scans[i] = result
+                    self._unplaced_scan_indices.remove(i)
+                    print(f'Placed scan {i}: location is {result.position}')
+                    return True
+                else:
+                    self._failed_matches.add((i,j))
+        print('Failed to find match')
+        return False
+
+
+@dataclass
+class PlacedScan:
+    position: np.array
+    beacons: set(tuple([int, int, int]))
+
+
+def try_to_fit(unrotated_beacon_set, ps):
+    for r in ROTATIONS:
+        rotated_beacon_set = rotate(r, unrotated_beacon_set)
+        result = try_to_fit_rotated(rotated_beacon_set, ps)
+        if result:
+            return result
+    return None
+
+
+def try_to_fit_rotated(rotated_beacon_set, ps):
+    potential_shifts = Counter()
+    for b1 in rotated_beacon_set:
+        for b2 in ps.beacons:
+            potential_shifts[tuple(b2-b1)] += 1
+    s, cnt = potential_shifts.most_common(1)[0]
+    if cnt >= 12:
+        s = np.array(s)
+        return PlacedScan(s, shift(rotated_beacon_set, s))
+    else:
+        return None
+
+
+def rotate(r, unrotated_beacon_set):
+    return list(np.matmul(r, b) for b in unrotated_beacon_set)
+
+
+def shift(rotated_beacon_set, vec):
+    return set(tuple(b + vec) for b in rotated_beacon_set)
+
 
 ROTATIONS = list(map(np.array,
                 [
@@ -27,97 +135,3 @@ ROTATIONS = list(map(np.array,
                     [[0,0,1],[0,1,0],[-1,0,0]], [[0,0,1],[0,-1,0],[1,0,0]],
                     [[0,0,-1],[0,1,0],[1,0,0]], [[0,0,-1],[0,-1,0],[-1,0,0]]
                 ]))
-
-
-class PlacedScan:
-    def __init__(self, position, beacons):
-        self.position = position
-        self.beacons = beacons
-
-
-def solution(day, lines):
-    original_scans = scans(lines)
-    placed_scans = {0: PlacedScan(np.array((0,0,0)), original_scans[0])}
-    unplaced_scan_indices = set(range(1,len(original_scans)))
-    failed_matches = set()
-    while unplaced_scan_indices:
-        found_match = False
-        for i in sorted(unplaced_scan_indices):
-            if found_match:
-                break
-            for j, ps in placed_scans.items():
-                if (i,j) in failed_matches:
-                    continue
-                result = try_to_fit(original_scans[i], ps)
-                if result:
-                    found_match = True
-                    placed_scans[i] = result
-                    unplaced_scan_indices.remove(i)
-                    print(f'Placed scan {i}: location is {result.position}')
-                    break
-                else:
-                    failed_matches.add((i,j))
-        if not found_match:
-            print(f'Failed to find match')
-            return
-    print(f'Placed all scans!')
-
-    all_beacons = reduce(set.union, map(lambda ps: ps.beacons, placed_scans.values()))
-    print(f'Found {len(all_beacons)} beacons')
-
-    max_manhattan_distance = 0
-    for ps1 in placed_scans.values():
-        for ps2 in placed_scans.values():
-            distance = sum(abs(ps1.position - ps2.position))
-            if distance > max_manhattan_distance:
-                max_manhattan_distance = distance
-    print(f'Max Manhattan distance: {max_manhattan_distance}')
-
-
-def scans(lines):
-    original_scans = []
-    latest_scan = None
-    for l in lines:
-        m = HEADER_LINE.match(l)
-        if m:
-            latest_scan = set()
-            continue
-        m = BEACON_LINE.match(l)
-        if m:
-            latest_scan.add((int(m.group(1)), int(m.group(2)), int(m.group(3))))
-            continue
-        # presumably blank line
-        original_scans.append(latest_scan)
-        latest_scan = None
-    if latest_scan is not None:
-        original_scans.append(latest_scan)
-    return original_scans
-
-
-def try_to_fit(unrotated_beacon_set, ps):
-    for r in ROTATIONS:
-        rotated_beacon_set = rotate(r, unrotated_beacon_set)
-        result = try_to_fit_rotated(rotated_beacon_set, ps)
-        if result:
-            return result
-    return None
-
-
-def try_to_fit_rotated(rotated_beacon_set, ps):
-    potential_shifts = Counter()
-    for b1 in rotated_beacon_set:
-        for b2 in ps.beacons:
-            potential_shifts[tuple(b2-b1)] += 1
-    for s, cnt in potential_shifts.items():
-        if cnt >= 12:
-            s = np.array(s)
-            return PlacedScan(s, shift(rotated_beacon_set, s))
-    return None
-
-
-def rotate(r, unrotated_beacon_set):
-    return list(np.matmul(r, b) for b in unrotated_beacon_set)
-
-
-def shift(rotated_beacon_set, vec):
-    return set(tuple(b + vec) for b in rotated_beacon_set)
